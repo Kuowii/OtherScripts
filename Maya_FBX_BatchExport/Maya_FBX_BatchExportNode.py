d39e098489b3a0f2b580eab8659394d0369358f8
Wings <pgw00k@163.com>
2022-07-19 15:15:21 +0800

    [Add]新增一个批量修改节点名字的Maya脚本 [Add]新增一个批量导出节点的Maya脚本

---
+++ Maya_FBX_BatchExport/Maya_FBX_BatchExportNode.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import maya.cmds as cmds
+import maya.mel as mel
+import os
+import threading
+
+class BatchExportWindow:
+    def __init__(self):
+       try:
+           cmds.deleteUI("BatchExportWindow");
+       except :
+           print('close BatchExportWindow');
+           self.version = "Batch Export v1.0";
+       self.buildUI();
+           
+    def buildUI(self):
+        self.MainWin=cmds.window(self.version);
+        cmds.window( self.MainWin, edit=True, widthHeight=(400, 150) );
+        cmds.columnLayout();
+        cmds.text(self.version);
+        cmds.rowColumnLayout( numberOfColumns=3, columnAttach=(1, 'left', 0), columnWidth=[(1, 50), (2, 300),(3, 50)] );
+        cmds.text( label='导出路径' )
+        self.txt_out = cmds.textField();
+        cmds.button(label=" 浏 览 ",command=self.browsrOutput);
+        cmds.button(label=" 导 出 ",command=self.startExport);
+        cmds.showWindow(self.MainWin);
+        
+    def startExport(self,sender):
+        print("Start Export");
+        out = cmds.textField(self.txt_out,tx=1,q=1); 
+        sel=[];
+        for s in cmds.ls(sl=1):
+            sel.append(s);
+        for s in sel:
+            cmds.select(s);
+            fp = os.path.join(out,s+".fbx")
+            cmds.file(fp,es=True,f=True,type="FBX export");
+
+    def browsrOutput(self,sender):
+        save_folder = cmds.fileDialog2(caption="选择输出目录",fm=3)[0];
+        cmds.textField(self.txt_out,tx=save_folder,edit=1);
+
+BatchExportWindow();
