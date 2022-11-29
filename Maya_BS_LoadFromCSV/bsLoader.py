c37231b562bccced87928ad5fb5e5842b5b479ba
Wings <gwpei@adiadigital.com>
2022-11-29 16:43:55 +0800

    [Update][Maya_BS_LoadFromCSV]添加进度条和提示框等

--- Maya_BS_LoadFromCSV/bsLoader.py
+++ Maya_BS_LoadFromCSV/bsLoader.py
@@ -23,7 +23,6 @@ class BSControlData:
     def setKeyFrame(self,kf,v):
         i=0;
         for b in self.bsBase:
-            print()
             cmds.setKeyframe('{0}.w[{1}]'.format(b,self.bsIndex[i]),t=kf,v=v);
             i = i+1;
         
@@ -65,5 +64,46 @@ def main():
         if bs.name in con:
             for i in  range(len(bs.KeyValues)):
                 con[bs.name].setKeyFrame(i,float(bs.KeyValues[i]));
+                
+def main2():
+    fp=cmds.fileDialog2(fileFilter="*.csv",caption="Select Facial Animation Data",fm=1)[0];
+    bss = maya.utils.executeInMainThreadWithResult(loadBSFromCSV,fp );
+    con = maya.utils.executeInMainThreadWithResult( getAllBSControl );
+        
+    keyCount = len(bss[2].KeyValues);
+    dataCount = len(bss);
     
-main(); 
+    cmds.playbackOptions(max=keyCount)
+    print(f'Get {dataCount} * {keyCount} data.');
+    
+    progressWin = cmds.window('Wings BS Loader')
+    cmds.rowLayout(numberOfColumns=2)
+    progressControl = cmds.progressBar(width=300)
+    cmds.button( label='Cancel', command='cmds.progressBar(progressControl, edit=1, isCancelled=1)' )
+    cmds.showWindow( progressWin )
+    cmds.progressBar( progressControl,
+                                edit=True,
+                                beginProgress=True,
+                                isInterruptable=True,
+                                status='Loading BlendShape...',
+                                maxValue=keyCount*dataCount
+                                )  
+    for bs in bss:
+        if cmds.progressBar(progressControl, query=True, isCancelled=True ):
+            break;
+        if bs.name in con:
+            for i in  range(len(bs.KeyValues)):
+                cmds.progressBar(progressControl, edit=True, step=1)          
+                con[bs.name].setKeyFrame(i,float(bs.KeyValues[i]));
+        else:
+            cmds.progressBar(progressControl, edit=True, step=keyCount)
+                            
+    print('Finish!')
+    cmds.progressBar(progressControl, edit=True, endProgress=True)
+    try:
+        cmds.deleteUI(progressWin);
+    except:
+        print(f'Error:');
+    cmds.confirmDialog( title='Wings BS Loader', message='Load Compelete!');
+    
+main2(); 
