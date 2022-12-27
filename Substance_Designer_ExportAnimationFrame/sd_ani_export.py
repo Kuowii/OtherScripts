b586100d0c4cd83570a6f14b0fff54582ddaae8f
Wings <pgw00k@163.com>
2022-12-27 14:53:00 +0800

    [Update]SD Animation Export

--- Substance_Designer_ExportAnimationFrame/sd_ani_export.py
+++ Substance_Designer_ExportAnimationFrame/sd_ani_export.py
@@ -10,10 +10,10 @@ def main():
     min_value = 0;
     max_value = 1;
     difference = max_value-min_value;
-    frames = 5;
+    frames = 30;
     step = difference/frames;
     
-    ids=['spread','pattern_scale'];
+    ids=['age'];
     folder_path='D:\\SD_Animsets\\'
     file_name = 'SD_Anim_Frame'
     file_type='\*.png'
@@ -24,17 +24,19 @@ def main():
     graph = sd_ui_mgr.getCurrentGraph();
     
     category = SDPropertyCategory.Input;
+    #print(dir(graph));
     ls = [];
     for id in ids:
         pi = graph.getPropertyFromId(id,category);
         ls.append(pi);
-        param = graph.getInputParameter(id);
-        print(dir(param));
+        
+        #param = graph.getInputParameter(id);
+        #print(dir(param));
 
-    # for frame in range(frames):
-        # for label_property in ls:
-            # graph.setPropertyValue(label_property,SDValueFloat.sNew(input_value));
-            # export.exportSDGraphOutputs(graph,folder_path,'{0}_{1}.png'.format(file_name,str(frame)));
-        # input_value = round((input_value + step),5);
+    for frame in range(frames):
+        for label_property in ls:
+            graph.setPropertyValue(label_property,SDValueFloat.sNew(input_value));
+            export.exportSDGraphOutputs(graph,folder_path,'{0}_{1}.png'.format(file_name,str(frame)));
+        input_value = round((input_value + step),5);
 
 main();
