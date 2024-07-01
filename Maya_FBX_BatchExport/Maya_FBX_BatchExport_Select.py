5ed36bdaec703c65b77005b0755d867ec3b9f5a6
Wings <pgw00k@outlook.com>
2024-07-01 17:07:48 +0800

    [Wings] 新增烘焙动画，改选择输出

--- Maya_FBX_BatchExport/Maya_FBX_BatchExport_Select.py
+++ Maya_FBX_BatchExport/Maya_FBX_BatchExport_Select.py
@@ -3,13 +3,68 @@ import maya.mel as mel
 import os
 import threading
 
+def remove_namespace(name):
+    if ':' in name:
+        return name.split(':')[-1]
+    return name
+def lastPath(name):
+    if '|' in name:
+        return name.split('|')[-1]
+    return name
+    
+def copy_animation_by_name(from_root, to_root):
+    start_frame = cmds.playbackOptions(query=True, minTime=True)
+    end_frame = cmds.playbackOptions(query=True, maxTime=True)
+    from_joints = cmds.listRelatives(from_root, allDescendents=True, type='joint', fullPath=True)
+    from_joints.append(from_root)
+    
+    to_joints = cmds.listRelatives(to_root, allDescendents=True, type='joint', fullPath=True)
+    to_joints.append(to_root)
+    
+    to_joints_dict = {lastPath(cmds.ls(jnt, shortNames=True)[0]): jnt for jnt in to_joints}
+    
+    count = 0
+    for from_joint in from_joints:
+        from_joint_name = cmds.ls(from_joint, shortNames=True)[0]
+        from_joint_name=remove_namespace(from_joint_name)
+        
+        if from_joint_name in to_joints_dict:
+            to_joint = to_joints_dict[from_joint_name]
+            anim_curves = cmds.keyframe(from_joint, query=True, name=True)
+            count=count+1
+            if not anim_curves:
+                continue
+            
+            for anim_curve in anim_curves:
+                anim_data = cmds.keyframe(anim_curve, query=True, time=(start_frame, end_frame), valueChange=True, timeChange=True)
+                for time, value in zip(anim_data[0::2], anim_data[1::2]):
+                    cmds.setKeyframe(to_joint, time=time, value=value, attribute=anim_curve.split('_')[-1])
+    
+    print(f"Copy {from_root} To {to_root} Count:{count}。")
+
 def ExportFileToFBX(sfile,ofile):
+    cmds.file(sfile, open=True,force=True);
+    rootbone='SM_female_01_a3_rig_rebinding:DHIbody:root'
     try:
-        cmds.file(sfile, open=True,force=True);
-        cmds.file(ofile,ea=True,f=True,type="FBX export");
+        cmds.select(rootbone,hi=True);
+        nsel = cmds.ls(cmds.ls(sl=True), type="joint")
+
+        # 获取时间滑块的开始和结束帧
+        start_frame = cmds.playbackOptions(query=True, minTime=True)
+        end_frame = cmds.playbackOptions(query=True, maxTime=True)
+        
+        # 烘焙骨骼动画
+        cmds.bakeResults(rootbone, time=(start_frame, end_frame),hi='below',sm=True,dic=True,pok=True,mr=True)
+        newroot = '|'+cmds.duplicate(rootbone)[0];
+        copy_animation_by_name(rootbone,newroot);
+        cmds.select(newroot,hi=True);
+    except Exception as err:
+        print(sfile+' Scene Exception : '+str(err));
+    try:
+        cmds.file(ofile,es=True,f=True,type="FBX export");
         return True
     except Exception as err:
-        print(sfile+' error : '+str(err));
+        print(sfile+' Export Exception : '+str(err));
         return False
 
 def DoBatchExport():
