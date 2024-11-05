10a5833de912c193f1f370bace1b5476d200d3a7
Wings <pgw00k@outlook.com>
2024-11-05 14:10:01 +0800

    新增射线检测确保轴吸附到模型上

--- Maya_PivotPreset/Maya_PivotPreset.py
+++ Maya_PivotPreset/Maya_PivotPreset.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import maya.cmds as cmds
+import maya.api.OpenMaya as om
 
 def get_bottom_center(obj):
     bbox = cmds.exactWorldBoundingBox(obj)
@@ -19,18 +20,55 @@ def get_bottom_center(obj):
     [center_x,min_y,max_z],
     [max_x,min_y,max_z],
     ]
-    return 
+    return
+    
+def get_all_mesh_children(parent_node):
+    children = cmds.listRelatives(parent_node, children=True, fullPath=True)
+    mesh_children = []
+    if children:
+        for child in children:
+            if cmds.nodeType(child) == 'mesh':
+                mesh_children.append(child)
+            elif cmds.nodeType(child) == 'transform':
+                mesh_children.extend(get_all_mesh_children(child))
+    return mesh_children
+    
+def find_nearest_point_on_mesh(obj,sp,ep):
+    meshes = get_all_mesh_children(obj)
+    start_point = om.MFloatPoint(sp[0], sp[1], sp[2])
+    end_point = om.MFloatPoint(ep[0], ep[1], ep[2])
+    min_distance = float('inf')
+    nearest_point = sp
+    direction = end_point-start_point
+    
+    omlist = om.MSelectionList()
+    for mesh in meshes:
+        omlist.add(mesh)
+        dag_path = omlist.getDagPath(0)
+        fn_mesh = om.MFnMesh(dag_path)
+        hit_result = fn_mesh.closestIntersection(
+        start_point, direction,
+        om.MSpace.kWorld, 9999, False)
+        omlist.clear()
+        if hit_result:
+            hit_point = hit_result[0]
+            hit_distance = hit_result[1]
+            if hit_distance < min_distance:
+                nearest_point = [hit_point[0],hit_point[1],hit_point[2]]
+    return nearest_point 
     
 
-def update_pivot(idx):
+def update_pivot(idx,isAttach=False):
     obj = cmds.ls(selection=True)[0]
-    x,y,z=get_bottom_center(obj)[idx]
-    print(f"New Pivot[{x},{y},{z}]")
+    points = get_bottom_center(obj)
+    sp=points[idx]
+    if isAttach:
+        sp = find_nearest_point_on_mesh(obj,sp,points[4])
+    x,y,z=sp
     cmds.xform(obj, piv=[x,y,z],ws=True)
-    #cmds.setAttr(f"{obj}.translate", x, -y, z)
     cmds.move(-x, -y, -z, obj, relative=True)
     cmds.makeIdentity(obj, apply=True,t=1 )
-    
+    cmds.delete(obj, ch=True)
 
 def onMayaDroppedPythonFile(*args):
     main()
@@ -52,13 +90,15 @@ class PivotPresetWindow:
             cb = cmds.checkBox(label='',changeCommand=lambda _, idx=i: self.on_checkbox_selected(idx))
             self.CheckBoxes.append(cb)
         cmds.checkBox(self.CheckBoxes[4], edit=True, value=True)
+        self.sel_id = 4
         cmds.setParent("..")
+        self.cbAttachMesh = cmds.checkBox(label='吸附到模型')
         cmds.button( label='应用', command=self.apply)
         cmds.showWindow(self.win)
         
     def apply(self,sender):
-        print(f"Select {self.sel_id}")
-        update_pivot(self.sel_id)
+        isAttach = cmds.checkBox(self.cbAttachMesh,query=True,value=True)
+        update_pivot(self.sel_id,isAttach)
         
     def on_checkbox_selected(self,idx):
         self.sel_id = idx
