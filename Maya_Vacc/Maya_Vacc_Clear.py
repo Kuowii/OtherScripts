6342d10d0512e74ede484527779d024b1215e81c
Wings <pgw00k@outlook.com>
2025-07-03 17:02:28 +0800

    更新清理脚本

--- Maya_Vacc/Maya_Vacc_Clear.py
+++ Maya_Vacc/Maya_Vacc_Clear.py
@@ -1,33 +1,21 @@
+# -*- coding: utf-8 -*-
 import maya.cmds as cmds
-
-class MessageBox: 
-   def __init__(self, info,title="MessageBox",ww=300,wh=100):
-       try:
-           cmds.deleteUI(title);
-       except :
-           print('close MessageBox');
-       self.win = cmds.window("MessageBox", widthHeight=(ww, wh),s=False)
-       cmds.columnLayout();
-       btn = cmds.button(label=info, w=ww, h=wh,c=self.close);
-       cmds.showWindow(self.win);
-          
-   def close(self,sender):
-       try:
-           cmds.deleteUI(self.win);
-       except :
-           print('Error on close.');
-
 aa=cmds.ls();
 isVir = False;
 for a in aa:
-    if "vaccine_gene" in a:
+    b=a.lower()
+    if "vacc" in b:
         cmds.delete(a);
         isVir = True;
-    if "breed_gene" in a:
+    if "breed" in b:
+        cmds.delete(a);
+        isVir = True;
+    if "virus" in b:
         cmds.delete(a);
         isVir = True;
                 
 if isVir:
-    MessageBox("Çå³ý³É¹¦");
+    cmds.file(modified=True)
+    cmds.confirmDialog( title='Wings Tool', message="å·²æ¸…é™¤è„šæœ¬", button=['OK'], defaultButton='OK')
 else:
-    MessageBox("Î´¸ÐÈ¾");
+    cmds.confirmDialog( title='Wings Tool', message="æœªæ„ŸæŸ“", button=['OK'], defaultButton='OK')
